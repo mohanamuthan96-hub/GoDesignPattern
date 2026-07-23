ae9cb6cbb0c22e2ea574c18c435582b4ac284bb4
Mohan_Amuthan <mohanamuthan96@gmail.com>
2026-07-23 09:54:45 +0530

    Remove Adapter pattern and add greeting message
    Removed the Adapter Design pattern implementation and added a greeting message.

--- main.go
+++ main.go
@@ -2,47 +2,6 @@ package main
 
 import "fmt"
 
-//Adapter Design pattern - Extended Implementation
-
-//Target interface
-type mobile interface {
-	chargeAppleMobile()
-}
-
-//Concrete Prototype
-type apple struct{}
-
-func (a *apple) chargeAppleMobile() {
-	fmt.Println("Apple Mobile is charging")
-}
-
-//adaptee
-type android struct{}
-
-func (a *android) chargeAndroidMobile() {
-	fmt.Printf("Charging android mobile")
-}
-
-//adapter - extend the functionality
-type androidadapter struct {
-	android *android
-}
-
-func (ad *androidadapter) chargeAppleMobile() {
-	ad.android.chargeAndroidMobile()
-}
-
-//client
-type client struct{}
-
-func (c *client) chargeMobile(mob mobile) {
-	mob.chargeAppleMobile()
-}
-func main() {
-	apple := &apple{}
-	client := &client{}
-	client.chargeMobile(apple)
-	android := &android{}
-	androidadapter := &androidadapter{android: android}
-	client.chargeMobile(androidadapter)
+func main(){
+	fmt.Println("Hello, Design Pattern")
 }
