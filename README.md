5414b3e871f3d4f868f310cb8092f17dde73d3f4
Mohan_Amuthan <mohanamuthan@gmail.com>
2026-07-23 12:32:01 +0530

    Singletonfactory pattern

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Learn Design Pattern in Golang
 1.Adapter Pattern - structural
 2.State Pattern - Behaviour
+3.singleton pattern - creational
--- main.go
+++ main.go
@@ -1,6 +1,37 @@
 package main
 
-import "fmt"
-func main(){
-	fmt.Println("Hello Design Pattern")
+import (
+	"fmt"
+	"sync"
+)
+
+type configure struct {
+	//configuration
+}
+
+var configureinstance *configure
+var lock = &sync.Mutex{}
+var counter int
+
+func getConfigureInstances() *configure {
+	if configureinstance == nil {
+		(*lock).Lock()
+		defer lock.Unlock()
+		if configureinstance == nil {
+			fmt.Println("Creating a instance of singleton")
+			counter = counter + 1
+			configureinstance = &configure{}
+		} else {
+			fmt.Println("Instance already created, counter is", counter)
+		}
+	} else {
+		fmt.Println("Instance already created, counter is", counter)
+	}
+	return configureinstance
+}
+func main() {
+	for range 50 {
+		go getConfigureInstances()
+	}
+	fmt.Scanln()
 }
