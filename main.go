470781ccb26c32cda89daa1c72f619c70ab1b378
Mohan_Amuthan <mohanamuthan@gmail.com>
2026-07-23 14:54:24 +0530

    Factory Pattern

--- main.go
+++ main.go
@@ -1,6 +1,42 @@
 package main
 
-import "fmt"
-func main(){
-	fmt.Println("Hello Design Pattern")
+import "log"
+
+type animal interface {
+	sound()
+}
+
+type cat struct {
+}
+
+func newcat() cat {
+	return cat{}
+}
+func (cat cat) sound() {
+	log.Print("Meow")
+}
+
+type dog struct {
+	name string
+}
+
+func newDog(name string) *dog {
+	return &dog{}
+}
+func (dog dog) sound() {
+	log.Print("wow")
+}
+
+func farm(sound int) {
+	var animal animal
+	if sound > 30 {
+		animal = newDog("Dogh")
+	} else {
+		animal = newcat()
+	}
+	animal.sound()
+}
+func main() {
+	//Decibel greater 30 is cat else dog
+	farm(54)
 }
